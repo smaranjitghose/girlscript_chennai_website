c042bf884602e3c7d50e570a0849b27162cf006d
Mustafiz04 <kaifee000@gmail.com>
2020-03-24 09:30:28 +0530

    Modified footer as per #70

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+mybutton = document.getElementById("toTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
