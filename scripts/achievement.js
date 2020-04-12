05e92472a067dbe82cc2a3d7501eec8c6ecb6054
unknown <ashishnagpal2498@gmail.com>
2020-04-12 16:56:56 +0530

    :star: icons corrected

--- scripts/achievement.js
+++ scripts/achievement.js
@@ -11,21 +11,34 @@ $(document).ready(function(){
     }
 
     if ($("div.achievement-card").hasClass("showing")) {
-      // a card is already in view
-      $("div.project-card.show")
-        .removeClass("show");
+	  // a card is already in view
+	  let button = $("i.fa-chevron-up");
+		button.removeClass('fa-chevron-up')
+		button.addClass('fa-chevron-down')
+	 let div = $("div.project-card.show")
+		div.removeClass('show');
 
       if (isShowing) {
         // this card was showing - reset the grid
         $("div.achievement-card")
-          .removeClass("showing");
+		  .removeClass("showing");
+		$("#proshow").removeClass("margin-button")
+		$('#probtn2').removeClass('margin-button2')  
+		let button = $($(this)[0].children[1].children[0]);
+		button.empty();
+		button.append(`<i class="fa fa-chevron-down"></i>`)	
       } else {
         // this card isn't showing - get in with it
         $(this)
           .css({zIndex: zindex})
-          .addClass("show");
-
-      }
+		  .addClass("show");
+		$('#proshow').addClass('margin-button');
+		$('#probtn2').addClass('margin-button2');
+		let button = $($(this)[0].children[1].children[0]);
+		button.empty();
+		button.append(`<i class="fa fa-chevron-up"></i>`)	
+	
+	}
 
       zindex++;
 
@@ -35,8 +48,12 @@ $(document).ready(function(){
         .addClass("showing");
       $(this)
         .css({zIndex:zindex})
-        .addClass("show");
-
+		.addClass("show");
+		$('#proshow').addClass('margin-button');
+		$('#probtn2').addClass('margin-button2'); 
+		let button = $($(this)[0].children[1].children[0]);
+		button.empty();
+		button.append(`<i class="fa fa-chevron-up"></i>`)	
       zindex++;
     }
     
