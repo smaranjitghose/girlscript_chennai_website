05d47fcb1a2946500d428e1e5575ee76ddb5afde
unknown <ashishnagpal2498@gmail.com>
2020-04-20 16:16:47 +0530

    :up: refactor contactUs JS

--- scripts/contactUs.js
+++ scripts/contactUs.js
@@ -2,9 +2,8 @@
 function removeSpaces(string) {
 	return string.split(' ').join('');
 }
-function ValidCaptcha() {
-	var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
-	var str2 = removeSpaces(document.getElementById('CaptchaInput').value);
+function ValidCaptcha(str2) {
+	let str1 = removeSpaces(document.getElementById('CaptchaDiv').value);
 	if (str1 == str2) {
 		return true;
 	} else {
@@ -12,76 +11,59 @@ function ValidCaptcha() {
 	}
 }
 function validateForm() {
-	var nameVal = document.myForm.name.value;
-	var numbers = /^[0-9]+$/;
-	if(nameVal.match(numbers)|| nameVal === ""){
-	  alert(nameVal==="" ? "Please enter name" : "Please enter correct name")
-	  document.myForm.name.focus();
-	  return false;
-	}
+	let name = document.myForm.name,
+		emailId = document.myForm.email,
+		subject = document.myForm.subject,
+		message = document.myForm.message,
+		atpos = emailId.value.indexOf("@"),
+		dotpos = emailId.value.lastIndexOf("."),
+		captchaInput = document.myForm.captchaInput,
+		numbers = /^[0-9]+$/;
 
-	var emailID = document.myForm.email.value;
-	atpos = emailID.indexOf("@");
-	dotpos = emailID.lastIndexOf(".");
-
-	var emailID = document.myForm.email.value;
-	atpos = emailID.indexOf("@");
-	dotpos = emailID.lastIndexOf(".");
-		 
-	if (atpos < 1 || ( dotpos - atpos < 2 )) {
-	  alert("Please enter correct email ID")
-	  document.myForm.email.focus() ;
-	  return false;
-	  }
-
-	var subject = document.myForm.subject.value;
-	var message = document.myForm.message.value;
-	if(subject.length < 10) {
-			alert( "Minimum Length of subject must be 10" );
-			document.myForm.subject.focus() ;
-			return false;
-		 }
-	if(message.length < 10) {
-			alert( "Minimum Length of message must be 10" );
-			document.myForm.message.focus() ;
-			return false;
-		 }
-
-	  return true;
-  }
-  
-  function checkform(theform) {
-	var why = "";
-	if (theform.CaptchaInput.value == "") {
-		why += "- Please Enter CAPTCHA Code.\n";
-	}
-	if (theform.CaptchaInput.value != "") {
-		if (ValidCaptcha(theform.CaptchaInput.value) == false) {
-			why += "- The CAPTCHA Code Does Not Match.\n";
-		}
-	}
-	if (why != "") {
-		alert(why);
+	if (name.value.match(numbers) || name.value === "") {
+		alert(name.value === "" ? "Please enter name" : "Please enter correct name")
+		name.focus();
 		return false;
 	}
-	else{
-		return true;
+	else if (atpos < 1 || (dotpos - atpos < 2)) {
+		alert("Please enter correct email ID")
+		emailId.focus();
+		return false;
 	}
-}
-$(function(){
-	var a = Math.ceil(Math.random() * 9) + '';
-	var b = Math.ceil(Math.random() * 9) + '';
-	var c = Math.ceil(Math.random() * 9) + '';
-	var d = Math.ceil(Math.random() * 9) + '';
-	var e = Math.ceil(Math.random() * 9) + '';
 
-	var code = a + b + c + d + e;
-	document.getElementById("txtCaptcha").value = code;
-	document.getElementById("CaptchaDiv").innerHTML = code;
+	else if (subject.value.length < 10) {
+		alert("Minimum Length of subject must be 10");
+		subject.focus();
+		return false;
+	}
+	else if (message.value.length < 10) {
+		alert("Minimum Length of message must be 10");
+		message.focus();
+		return false;
+	}
+	else if (captchaInput.value === "") {
+		alert("Please Enter CAPTCHA Code.");
+		captchaInput.focus();
+		return false;
+	}
+	else if (!(ValidCaptcha(captchaInput.value))) {
+		alert("The CAPTCHA Code Does Not Match.");
+		captchaInput.focus();
+		return false;
+	}
+
+	return true;
+}
+
+$(function () {
+	let code = "";
+	for (let i = 0; i < 5; i++)
+		code += Math.ceil(Math.random() * 9);
+	document.getElementById("CaptchaDiv").value = code;
 
 	// Init AOS
 	AOS.init({
 		duration: 2000,
-	  });
+	});
 
 })
