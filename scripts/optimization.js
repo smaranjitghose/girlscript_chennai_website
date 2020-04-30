87d4335991adcdfd356ded7688f475a54d3061a7
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-04-30 12:37:35 +0530

    Navigation TImings API code

--- scripts/optimization.js
+++ scripts/optimization.js
@@ -1,3 +1,22 @@
 function perf(type, name, data, options = "") {
     console.log(`%c${type}: %c${name} | %c${data ? Math.round(data) : ''}ms %c${options}`, "color:red", "color:green", "color:blue", "color:lightblue")
 }
+
+window.addEventListener("load", () => {
+
+    // Navigation Timing API
+    const navEntries = performance.getEntriesByType("navigation");
+    navEntries.forEach(entry => {
+        perf("navigation", "fetch-start", entry.fetchStart);
+        const ttfb = entry.responseStart - entry.fetchStart;
+        perf("navigation", "ttfb", ttfb);
+    });
+
+    //old versions (fallback)
+    if (navEntries.length == 0) {
+        perf("navigation", "fetch-start", performance.timing.fetchStart);
+        const ttfb = performance.timing.responseStart - performance.timing.fetchStart;
+        perf("navigation", "ttfb", ttfb);
+    }
+
+});
