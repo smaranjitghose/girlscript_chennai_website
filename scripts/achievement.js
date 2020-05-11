8af4b59253a3196378c457b63ece47bb9fd511ad
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-11 20:52:53 +0530

    Uniform code base format based on prettier settings

--- scripts/achievement.js
+++ scripts/achievement.js
@@ -1,66 +1,61 @@
 function showMoreCards(DOMbtn) {
-	let btn = $(DOMbtn)
-	let showMore = btn.attr('show-more')
-	let moreCardDiv = $('#moreCards')
-	if (showMore == "false") {
+	let btn = $(DOMbtn);
+	let showMore = btn.attr('show-more');
+	let moreCardDiv = $('#moreCards');
+	if (showMore == 'false') {
 		btn.empty();
-		btn.append('Show Less')
-		btn.attr("show-more", "true")
-		moreCardDiv.removeClass("hidediv");
-	}
-	else {
+		btn.append('Show Less');
+		btn.attr('show-more', 'true');
+		moreCardDiv.removeClass('hidediv');
+	} else {
 		btn.empty();
-		btn.append("Show More");
-		btn.attr("show-more", "false")
+		btn.append('Show More');
+		btn.attr('show-more', 'false');
 		moreCardDiv.addClass('hidediv');
 		// Card opened from more cards - then -
-		console.log(moreCardDiv[0].parentElement)
-		let moreDivFlag = false
+		console.log(moreCardDiv[0].parentElement);
+		let moreDivFlag = false;
 		for (i of moreCardDiv[0].children) {
-			moreDivFlag = i.classList.value.includes("show") ? true : false;
-			i.classList.remove('show')
+			moreDivFlag = i.classList.value.includes('show') ? true : false;
+			i.classList.remove('show');
 			if (moreDivFlag) {
 				btn.removeClass('margin-button');
-				moreCardDiv[0].parentElement.classList.remove("showing");
+				moreCardDiv[0].parentElement.classList.remove('showing');
 				break;
 			}
-
 		}
 	}
-
 }
 function displayCard(domCard) {
 	event.preventDefault();
 	let clickedCard = $(domCard),
-		achievementCard = $("div.achievement-card"),
+		achievementCard = $('div.achievement-card'),
 		showBtn = $('#proshow'),
 		chevronButton = $(clickedCard[0].children[1].children[0]);
 
 	// Same Card Clicked
-	if (clickedCard.hasClass("show")) {
-		achievementCard.removeClass("showing");
-		clickedCard.removeClass("show")
+	if (clickedCard.hasClass('show')) {
+		achievementCard.removeClass('showing');
+		clickedCard.removeClass('show');
 		chevronButton.empty();
 		chevronButton.append(`<i class="fa fa-chevron-down"></i>`);
 		showBtn.removeClass('margin-button');
-	}
-	else if (achievementCard.hasClass("showing")) {
+	} else if (achievementCard.hasClass('showing')) {
 		//Some other card is already open -
 		// close that card and open current card;
-		let div = $("div.project-card.show"),
+		let div = $('div.project-card.show'),
 			button = $(div[0].children[1].children[0]);
 		div.removeClass('show');
 		button.empty();
-		button.append('<i class="fa fa-chevron-down"></i>')
-		clickedCard.addClass("show");
+		button.append('<i class="fa fa-chevron-down"></i>');
+		clickedCard.addClass('show');
 		chevronButton.empty();
-		chevronButton.append(`<i class="fa fa-chevron-up"></i>`)
-	}
-	else {
-		achievementCard.addClass("showing");
-		clickedCard.addClass("show");
+		chevronButton.append(`<i class="fa fa-chevron-up"></i>`);
+	} else {
+		achievementCard.addClass('showing');
+		clickedCard.addClass('show');
 		chevronButton.empty();
-		chevronButton.append(`<i class="fa fa-chevron-up"></i>`)
+		chevronButton.append(`<i class="fa fa-chevron-up"></i>`);
 		showBtn.addClass('margin-button');
 	}
 }
@@ -72,22 +67,30 @@ $(function () {
 	});
 
 	// Cards - WayPoint
-	$('.card--1').waypoint(function (direction) {
-		$('.card--1').addClass('animated bounceInLeft')
-	}, {
-		offset: '60%'
-	});
+	$('.card--1').waypoint(
+		function (direction) {
+			$('.card--1').addClass('animated bounceInLeft');
+		},
+		{
+			offset: '60%'
+		}
+	);
 
-	$('.card--2').waypoint(function (direction) {
-		$('.card--2').addClass('animated zoomIn')
-	}, {
-		offset: '60%'
-	});
+	$('.card--2').waypoint(
+		function (direction) {
+			$('.card--2').addClass('animated zoomIn');
+		},
+		{
+			offset: '60%'
+		}
+	);
 
-	$('.card--3').waypoint(function (direction) {
-		$('.card--3').addClass('animated bounceInRight')
-	}, {
-		offset: '60%'
-	});
-
+	$('.card--3').waypoint(
+		function (direction) {
+			$('.card--3').addClass('animated bounceInRight');
+		},
+		{
+			offset: '60%'
+		}
+	);
 });
