0551573d9829ee10da4c8110ce0ffbba684a6fb9
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-13 11:19:55 +0530

    Converting PAge Dats to JSON

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+
+const trendCardsDetails = [
+	{
+		backgroundCover: 'assets/Images/index/ml.jpg',
+		displayName: 'Machine Learning'
+	},
+	{
+		backgroundCover: 'assets/Images/index/IoT1.jpg',
+		displayName: 'Internet of Things'
+	},
+	{
+		backgroundCover: 'assets/Images/index/program.jpg',
+		displayName: 'Web Development'
+    },
+    {
+        backgroundCover: 'assets/Images/index/cloud1.jpg',
+        displayName: 'Cloud Computing'
+    },
+    {
+        backgroundCover: 'assets/Images/index/cs3.jpg',
+        displayName: 'Cyber Security'
+    },
+    {
+        backgroundCover: 'assets/Images/index/robot1.jpg',
+        displayName: 'Robotics'
+    }
+];
+
+
