4a78393e2aeee0b80cf5640671691974393bead6
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-15 10:20:25 +0530

    Converting data to JSON format

---
+++ scripts/announcement.js
@@ -0,0 +1,26 @@
+const recentEventsDetails = [
+	{
+		coverImage: 'assets/Images/announcement/producthunt.png',
+		eventName: 'Product Hunt',
+		altName: 'Product Hunt Logo',
+		eventDescription: `We are feeling much happy to announce Product Hunt as out official In-Kind partner.
+                                Thankyou for being with Girlscript and helping it make a success.`,
+		referencePage: '#!'
+	},
+    {
+        coverImage: 'assets/Images/announcement/contributors.png',
+        eventName: 'Contributions',
+        altName: 'Contributors Illustration',
+        eventDescription: `We are extremely elated to see so many participants coming up with different skills and
+								new ways of approaching solution and contribute to the project.`,
+        referencePage: '#!'
+    },
+    {
+        coverImage: 'assets/Images/announcement/DU-Express.png',
+        eventName: 'DU Express',
+        altName: 'DU Express Logo',
+        eventDescription: `We would like to thank DU Express (@DuExpress) to be our Media Partner for GirlScript
+								Summer Of Code 2020.`,
+        referencePage: '#!'
+    }
+];
