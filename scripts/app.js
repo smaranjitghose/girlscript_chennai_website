c6cfd988c410777c48d70248aae4177551eccb9b
aashimgarg <18bcs001@smvdu.ac.in>
2020-05-16 19:46:55 +0530

    improving the variable scoping

--- scripts/app.js
+++ scripts/app.js
@@ -240,8 +240,8 @@ function topBtnClick() {
 
 
 function init() {
-var imgDefer = document.getElementsByTagName('img');
-for (var i=0; i<imgDefer.length; i++) {
+let imgDefer = document.getElementsByTagName('img');
+for (let i=0; i<imgDefer.length; i++) {
 if(imgDefer[i].getAttribute('data-src')) {
 imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
 } } }
