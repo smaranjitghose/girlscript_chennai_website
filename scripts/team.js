b6f4e5cd81be6fc962e91e282daeee8b8413d195
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-16 18:57:43 +0530

    Team page hardcoded version completed

--- scripts/team.js
+++ scripts/team.js
@@ -7,7 +7,7 @@ const teamData = [
 		linkedIn: 'https://www.linkedin.com/in/smaranjitghose/',
 		github: 'https://github.com/smaranjitghose',
 		twitter:'',
-		image: ''
+		image: 'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
 	},
 
 	{
