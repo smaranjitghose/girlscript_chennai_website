d548f83639435beaa0b5f687689541363206a0d0
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-18 12:01:05 +0530

    Converting timeline to JSOn information for scalibility

--- scripts/announcement.js
+++ scripts/announcement.js
@@ -1,3 +1,25 @@
+const announcementTimeline = [
+	{
+		eventName: 'Girlscript Contributors Week',
+		eventDescription: `It is a meetup where everyone who are a part of Girlscript will gather and share their
+							experience.`,
+		eventDate: 'June 20, 2020',
+		eventPageLink: './event1.html'
+	},
+	{
+		eventName: 'Code Camp',
+		eventDescription: `It is a coding camp where everyone can participate and learn about different technologies.`,
+		eventDate: 'May 20, 2020',
+		eventPageLink: './event1.html'
+	},
+	{
+		eventName: 'Code Camp',
+		eventDescription: `It is a coding camp where everyone can participate and learn about different technologies.`,
+		eventDate: 'May 20, 2020',
+		eventPageLink: './event1.html'
+	}
+];
+
 const recentEventsDetails = [
 	{
 		coverImage: 'assets/Images/announcement/producthunt.png',
@@ -25,6 +47,31 @@ const recentEventsDetails = [
 	}
 ];
 
+const generateTimelineCard = (timelineDetail, position) => {
+	const { eventName, eventDescription, eventDate, eventPageLink } = timelineDetail;
+
+	const dateType = position === 'right' ? 'time-2' : 'time-1';
+
+	const timeline = `<div class="timeline-container ${position}">
+						<div class="timeline-content">
+							<h3>${eventName}</h3>
+							<hr class="mw-100 line1" />
+							<p class="description">
+								${eventDescription}
+							</p>
+							<div class="time ${dateType}">
+								<h5>${eventDate}</h5>
+							</div>
+							<div class="card-stats">
+								<a href="${eventPageLink}" class="btn card-btn card1-text" target="__blank">Read More</a>
+							</div>
+						</div>
+					</div>`;
+
+	const timelineSection = document.getElementById('timeline');
+	timelineSection.innerHTML += timeline;
+};
+
 const generateEventsCard = (eventDetail, fixClassIndex) => {
 	const { coverImage, eventName, altName, eventDescription, referencePage } = eventDetail;
 	const eventCard = `<div class="card r-events-card">
@@ -45,15 +92,23 @@ const generateEventsCard = (eventDetail, fixClassIndex) => {
 							<a href="${referencePage}" class="btn card-btn card1-text card-btn2"> Read More</a>
 						</div>
 					</div>`;
-	
+
 	const recentEventsSection = document.getElementById('eventCards');
 	recentEventsSection.innerHTML += eventCard;
 };
 
+const insertAnnouncementTimeline = () => {
+	// position of events on timeline will be judged automatically
+	announcementTimeline.forEach((details, index) => {
+		const position = index % 2 === 0 ? 'left' : 'right';
+		generateTimelineCard(details, position);
+	});
+};
 const insertEventCards = () => {
 	recentEventsDetails.forEach((detail, index) => {
 		generateEventsCard(detail, index + 1);
 	});
 };
 
+insertAnnouncementTimeline();
 insertEventCards();
