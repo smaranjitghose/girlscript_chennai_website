d8fe1c9fd5a659fb88cde295fd50917074998335
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-20 23:22:27 +0530

    Completed mkaing the upto card with closest hover effects

---
+++ src/components/UpToCard/index.js
@@ -0,0 +1,118 @@
+import styled from 'styled-components';
+
+const CardImage = styled.img`
+  border-radius: 5px;
+  flex-shrink: 0;
+  width: 100%;
+`;
+
+const CardBody = styled.div`
+  flex: 1 1 auto;
+  min-height: 1px;
+  padding: 1.25rem;
+  display: block;
+
+  > h5 {
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: normal;
+    font-size: 22px;
+    font-family: 'Quicksand', sans-serif;
+    margin-bottom: 0.75rem;
+    color: #f47621;
+  }
+
+  > p {
+    letter-spacing: 1px;
+    font-family: 'Comfortaa', sans-serif;
+    text-align: justify;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 26px;
+    margin: 0 0 15px 0;
+    color: #111111;
+  }
+  > a {
+    display: block;
+    text-align: right;
+    opacity: 0.6;
+    color: inherit;
+    -webkit-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: white;
+      opacity: 1;
+    }
+  }
+`;
+
+const CustomCard = styled.div`
+  cursor: pointer;
+  border: none;
+  position: relative;
+  overflow: hidden;
+  border-radius: 20px;
+  box-shadow: 0 10px 25px rgba(240, 112, 0, 0.23),
+    0 6px 0px rgba(240, 112, 0, 0.5);
+  padding: 1rem;
+  margin-bottom: 3rem;
+  word-wrap: break-word;
+  background-clip: border-box;
+  background-color: white;
+  z-index: 1;
+
+  &:before {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    top: -16px;
+    right: -16px;
+    background-color: #fff;
+    height: 74px;
+    width: 52px;
+    border-radius: 32px;
+    transition: transform 0.25s ease-out, -webkit-transform 0.25s ease-out;
+  }
+
+  &:hover:before {
+    transform: scale(22);
+    background-color: #f47621;
+  }
+
+  &:hover ${CardBody} {
+    > h5 {
+      color: white;
+    }
+    > p {
+      color: white;
+    }
+    > a {
+      color: white;
+    }
+  }
+`;
+
+const UpToCard = () => {
+  return (
+    <div className="col-xl-4 col-lg-6 col-md-6">
+      <CustomCard>
+        <CardImage src="/assets/images/achievements/ach1.jpg" alt="Hello" />
+        <CardBody>
+          <h5>Forbes 30 under 30</h5>
+          <p className="card-text ct1">
+            Congratulations to our founding director, Anubha Maneshwar for
+            making it to Forbes under 30 list of class 2020. 300 Entrepreneurs
+            made it to the list in 10 categories.
+          </p>
+          <a href="#!">
+            Read full story <i className="fa fa-arrow-right"></i>
+          </a>
+        </CardBody>
+      </CustomCard>
+    </div>
+  );
+};
+
+export default UpToCard;
--- src/pages/achievements/index.js
+++ src/pages/achievements/index.js
@@ -1,14 +1,50 @@
 import Head from 'next/head';
 import Layout from '../../components/Layout';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import UpToCard from '../../components/UpToCard';
+
+const Heading = styled.h2`
+  font-size: 38px;
+  letter-spacing: 3px;
+  text-align: center;
+  font-weight: bold;
+  padding-bottom: 3rem;
+  font-family: 'Baloo Bhaina 2', sans-serif;
+  > span {
+    color: #f47621;
+  }
+`;
 
 const AchievementsPage = () => {
+  const [uptoDetails, setDetails] = useState([]);
+
+  useEffect(() => {
+    const getDetails = async () => {
+      const res = await fetch('/membersUptoCards.json');
+      const data = await res.json();
+      setDetails(data);
+    };
+
+    getDetails();
+  }, []);
+
   return (
     <Layout>
       <Head>
         <title>GirlScript Website Boilerplate | Achievements</title>
       </Head>
 
-      <h2 className="center temp">This is Achievements Page</h2>
+      <div className="container space-out">
+        <Heading>
+          See What Our Other <span>Members Are Upto</span>
+        </Heading>
+        <div className="container row m-auto">
+          {uptoDetails.map((detail, index) => (
+            <UpToCard key={index} detail={detail} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
