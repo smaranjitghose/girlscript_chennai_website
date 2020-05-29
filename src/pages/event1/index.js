6acefd92b68e6610034d3f187c04afa2913ae78b
aashimgarg <18bcs001@smvdu.ac.in>
2020-05-29 15:01:25 +0530

    added the faqCard section

--- src/pages/event1/index.js
+++ src/pages/event1/index.js
@@ -3,6 +3,7 @@ import Layout from '../../components/Layout';
 import EventHeader from '../../components/event1/header';
 import EventScheduleSection from '../../components/event1/ScheduleSection';
 import EventAboutSection from '../../components/event1/AboutSection';
+import FAQCardSection from '../../components/event1/FaqSection';
 
 const eventsPage = () => {
   return (
@@ -13,6 +14,7 @@ const eventsPage = () => {
       <EventHeader />
       <EventAboutSection />
       <EventScheduleSection />
+      <FAQCardSection />
     </Layout>
   );
 };
