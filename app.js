c728752bb95c931b2fab8210607960bc9d22d45a
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-30 13:23:06 +0530

    Security headers

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
 const app = express();
+
+// Global Middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
 
 module.exports = app;
