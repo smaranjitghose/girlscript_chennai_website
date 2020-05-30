2fda7e48bbaa0bb59e301c9f31bf0e53e9654367
aashimgarg <18bcs001@smvdu.ac.in>
2020-05-30 19:06:52 +0530

    added styled components for the register section :fire:

---
+++ src/components/event1/RegisterSection/StyledRgisterHeading.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledRegisterHeading = styled.h1`
+
+font-family: 'Comfortaa', sans-serif;
+font-size: 70px;
+font-weight: 500;
+border-bottom: 1px solid rgba(100, 100, 100, 0.1);
+width: 300px;
+letter-spacing: 2px;
+color: #f47621;
+
+}
+
+> span {
+
+font-size: 50px;
+font-weight: lighter;
+color: #444;
+
+}
+
+`;
