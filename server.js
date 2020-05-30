375f7539d95dcd8153a9c8bf9a53fc709de39231
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-05-30 13:47:52 +0530

    Atlas setup and confuguration complete

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-// require the application
-const app = require("./app");
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+// require the main application
+const app = require('./app');
+
+// Connect to Main DB
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log('Database Successfully Connected!'))
+	.catch(() => console.log('Something is wrong in connecting DB'));
 
 // main server settings
 const PORT = process.env.PORT || 3000;
 
 // listen to port 3000
 const server = app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  console.log(`Application started on port:${PORT}`);
+	if (err) console.log(err);
+	console.log(`Application started on port:${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log(err.name, err.message);
+	server.close(() => {
+		console.log('Closing server and requests');
+		process.exit(1);
+	});
 });
